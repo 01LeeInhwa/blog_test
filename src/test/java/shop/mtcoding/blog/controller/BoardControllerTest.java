2a1acbf160a767367fa37b31cdb51513122366cd
Inhwa <2inhwa5@gmail.com>
2023-02-12 01:42:41 +0900

    게시글 삭제 및 테스트 완료

--- src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
+++ src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.sql.Timestamp;
@@ -104,6 +105,7 @@ public class BoardControllerTest {
         System.out.println("테스트 : " + responseBody);
 
         // then
+        resultActions.andExpect(jsonPath("$.code").value(1));
         resultActions.andExpect(status().isOk());
     }
 
